b877ad1515a400123a5956f879ba517a5cd57f04
Tony Bowden <tony@tmtm.com>
2015-06-24 15:55:24 +0100

    Initial scraper

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,41 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/bin/env ruby
+# encoding: utf-8
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'scraperwiki'
+require 'nokogiri'
+require 'open-uri'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read) 
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  noko.css('.MsoTableGrid tr').drop(1).each do |row|
+    tds = row.css('td')
+    data = { 
+      id: tds[0].text.strip,
+      name: tds[1].text.strip,
+      area: tds[2].text.strip,
+      photo: tds[3].css('img[src*="assembleenationale"]/@src').text,
+      party: "unknown",
+      term: 12,
+      #source: url,
+    }
+    puts data
+    ScraperWiki.save_sqlite([:name, :term], data)
+  end
+end
+
+term = {
+  id: 12,
+  name: '12th National Assembly',
+  start_date: '2013-12-21',
+}
+ScraperWiki.save_sqlite([:id], term, 'terms')
+
+scrape_list('http://www.assembleenationale.mr/index.php?option=com_content&view=article&id=352&Itemid=164&lang=en')
