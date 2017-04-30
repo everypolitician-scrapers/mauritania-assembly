faed16862525d16f9cdcf261f7d43aedad37a730
Tony Bowden <tony@tmtm.com>
2017-04-30 11:46:31 +0100

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,33 +1,34 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-# require 'pry'
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
 def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
 def scrape_list(url)
   noko = noko_for(url)
   noko.css('.MsoTableGrid tr').drop(1).each do |row|
     tds = row.css('td')
-    data = { 
-      id: tds[0].text.strip,
-      name: tds[1].text.strip,
-      area: tds[2].text.strip,
+    data = {
+      id:    tds[0].text.strip,
+      name:  tds[1].text.strip,
+      area:  tds[2].text.strip,
       photo: tds[3].css('img[src*="assembleenationale"]/@src').text,
-      party: "unknown",
-      term: 12,
-      #source: url,
+      party: 'unknown',
+      term:  12,
+      # source: url,
     }
-    # puts data
-    ScraperWiki.save_sqlite([:name, :term], data)
+    # puts data
+    ScraperWiki.save_sqlite(%i[name term], data)
   end
 end
 
